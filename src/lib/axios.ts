5eaee75e457086015496ab111c4e4324fbd3f24f
jeffchoi <ihello0720@gmail.com>
2019-01-17 23:30:53 +0900

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let API_URL = 'Xhttp://52.78.225.36/api';
+let API_URL = 'http://52.78.225.36/api';
 
 if (process.env.NODE_ENV === 'development') {
   API_URL = 'http://127.0.0.1:4000/api';
