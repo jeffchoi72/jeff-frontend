c85c5044779db670e4ae358551d8c62de0eb16bf
jeffchoi72 <ihello0720@gmail.com>
2019-06-27 00:34:24 +0900

    API 서버 주소를 변경한다

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,24 +1,24 @@
 import axios from 'axios';
 
-let API_URL = 'http://52.78.225.36:8080/api';
+let API_URL = "http://52.79.227.127:8080/api";
 
-if (process.env.NODE_ENV === 'development') {
-  API_URL = 'http://127.0.0.1:4000/api';
+if (process.env.NODE_ENV === "development") {
+  API_URL = "http://127.0.0.1:4000/api";
 }
 
 export const baseAxios = (() => {
   return axios.create({
     baseURL: API_URL,
-    timeout: 1000,
+    timeout: 1000
   });
 })();
 
-export const authAxios = ((token: string| null) => {
+export const authAxios = ((token: string | null) => {
   return axios.create({
     baseURL: API_URL,
     timeout: 1000,
     headers: {
-      'bearer-token': token,
+      "bearer-token": token
     }
-  })
-})(localStorage.getItem('token'));
+  });
+})(localStorage.getItem("token"));
